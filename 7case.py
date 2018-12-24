700c78ec71b19accc94dfc48a68854f80ce718f6
XeniaOlshanskaya <ks14012000@gmail.com>
2018-12-24 23:32:11 +0700

    def word + def dictionary + def sentence

--- 7case.py
+++ 7case.py
@@ -48,3 +48,39 @@ def dictionary(list_of_words, start_words, finish_words, sentanse):
         else:
             pass
     text(b, start_words, sentanse, finish_words)
+
+
+def text(b, start_words, sentanse, finish_words):
+    """ Function for sentence creating """
+    for n in range(sentanse):
+        sentanse_ = []
+        length = len(start_words) - 1
+        index_ = randint(0, length)
+        a = b.get(start_words[index_])
+        n = randint(0, len(a) - 1)
+        a = a[n]
+        sentanse_.append(start_words[index_])
+        sentanse_.append(a)
+        counter = 2
+        while sentanse_[-1] not in finish_words:
+            if counter < 5:
+                c = b.get(a)
+                n = randint(0, len(c) - 1)
+                c = c[n]
+                if c in finish_words:
+                    pass
+                else:
+                    sentanse_.append(c)
+                    counter += 1
+            elif 5 <= counter < 20:
+                c = b.get(a)
+                n = randint(0, len(c) - 1)
+                c = c[n]
+                sentanse_.append(c)
+                counter += 1
+            elif counter == 19:
+                d = randint(0, len(finish_words) - 1)
+                c = b.get(finish_words[d])
+                sentanse_.append(c)
+            a = c
+        print(str(sentanse_))

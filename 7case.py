d17ee754118ee156aac2d29d378f3ae31cb3a7d0
XeniaOlshanskaya <ks14012000@gmail.com>
2018-12-24 23:30:09 +0700

    def word + def dictionary

--- 7case.py
+++ 7case.py
@@ -25,3 +25,26 @@ def words(text, sentanse):
         if i.istitle():
             start_words.append(i)
     dictionary(list_of_words, start_words, finish_words, sentanse)
+
+
+def dictionary(list_of_words, start_words, finish_words, sentanse):
+    """ Function for dictionary """
+    d = []
+    for i in range(len(list_of_words)):
+        if list_of_words[i] not in finish_words:
+            d.append(list_of_words[i])
+    a = []
+    c = []
+    b = {}
+    for i in range(len(d)):
+        if d[i] not in c:
+            while d[i] in list_of_words:
+                k = list_of_words.index(d[i])
+                a.append(list_of_words[k + 1])
+                list_of_words.pop(k)
+                b.update({d[i]: a})
+            a = []
+            c.append(d[i])
+        else:
+            pass
+    text(b, start_words, sentanse, finish_words)
